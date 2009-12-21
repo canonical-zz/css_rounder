86a12b6672cdfd9efbf225a351c1c0e5bb328c50
Joe McGlynn <joemcglynn@yahoo.com>
2009-12-20 22:48:31 -0800

    added background color (needs to be paramaterized still) because transparency not working as expected

--- lib/commands.rb
+++ lib/commands.rb
@@ -1,35 +1,39 @@
 require 'rails_generator'
 require 'rails_generator/commands'
+require 'RMagick'
 
 module Canonical #:nodoc:
   module Generator #:nodoc:
     module Commands #:nodoc:
       module Create
         def generate_corner_graphics(radius, color, destination)
-          radius = Integer(radius)
+          radius = Integer(radius)-1
           size = radius * 2
 
-          canvas = Magick::Image.new(size, size)
+          canvas = Magick::Image.new(size, size)   {self.background_color = 'black'}
+          canvas.units = Magick::PixelsPerInchResolution
+          canvas.x_resolution = 72
+          canvas.y_resolution = 72
           gc = Magick::Draw.new
 
           # Draw ellipse
           gc.fill(color)
+          gc.fill_opacity 1
 
           gc.circle(radius-1, radius-1, radius-1, 0)
           gc.draw(canvas)
-          canvas.transparent_color = 'white'
 
           puts "      create " + File.join(destination, 'top-left.gif')
           canvas.crop(0, 0, radius, radius).write(File.join(destination, 'top-left.gif'))
 
           puts "      create " + File.join(destination, 'top-right.gif')
-          canvas.crop(radius-1, 0, radius, radius).write(File.join(destination, 'top-right.gif'))
+          canvas.crop(radius, 0, radius, radius).write(File.join(destination, 'top-right.gif'))
 
           puts "      create " + File.join(destination, 'bot-left.gif')
-          canvas.crop(0, radius-1, radius, radius).write(File.join(destination, 'bot-left.gif'))
+          canvas.crop(0, radius, radius, radius).write(File.join(destination, 'bot-left.gif'))
 
           puts "      create " + File.join(destination, 'bot-right.gif')
-          canvas.crop(radius-1, radius-1, radius, radius).write(File.join(destination, 'bot-right.gif'))
+          canvas.crop(radius, radius, radius, radius).write(File.join(destination, 'bot-right.gif'))
         end
       end
 
